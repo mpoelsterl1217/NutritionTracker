ba7470ef24ee65a1eefedb66aa0d88d9510bc793
Matthew Poelsterl <mpoelsterl1217@gmail.com>
2021-07-20 10:52:06 -0500

    Initial Commit

---
+++ NutritionTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NutritionTracker
+//
+//  Created by Matthew Poelsterl on 7/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
